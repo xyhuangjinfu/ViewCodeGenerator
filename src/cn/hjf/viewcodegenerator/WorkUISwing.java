e721bbbd9d2b47881418f6b1f47ef61564b0bb29
huangjinfu <630569027@qq.com>
2015-09-29 22:00:51 +0800

    界面实现替换为Swing

---
+++ src/cn/hjf/viewcodegenerator/WorkUISwing.java
@@ -0,0 +1,139 @@
+package cn.hjf.viewcodegenerator;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
+
+public class WorkUISwing {
+    
+    private JFrame mMainFrame;
+    private JButton mGenerateButton;
+    private JButton mChooseJavaButton;
+    private JButton mChooseXmlButton;
+    private JLabel mJavaPathLabel;
+    private JLabel mXmlPathLabel;
+    private JFileChooser mJavaFileChooser;
+    private JFileChooser mXmlFileChooser;
+//    private JPanel
+    
+    private CodeGenerator mCodeGenerator;
+    private OS mOS;
+    
+    public WorkUISwing() {
+        mCodeGenerator = new CodeGenerator();
+        mOS = new OS();
+    }
+    
+    public void show() {
+        mMainFrame = new JFrame("代码生成器");
+        mMainFrame.setSize(1000, 500);
+        mMainFrame.setLayout(new GridLayout(3, 2));
+        mMainFrame.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+            }
+            @Override
+            public void windowIconified(WindowEvent e) {
+            }
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+            }
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+            }
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+            @Override
+            public void windowClosed(WindowEvent e) {
+            }
+            @Override
+            public void windowActivated(WindowEvent e) {
+            }
+        });
+        
+        mGenerateButton = new JButton("生成代码");
+        mGenerateButton.setSize(100, 100);
+        mChooseJavaButton = new JButton("选择Java文件(Activity等)");
+        mChooseJavaButton.setSize(100, 100);
+        mChooseXmlButton = new JButton("选择Xml文件(布局文件)");
+        mChooseXmlButton.setSize(100, 100);
+        
+        mJavaPathLabel = new JLabel("Java文件路径");
+        mXmlPathLabel = new JLabel("Xml文件路径");
+        
+        mMainFrame.add(mChooseJavaButton);
+        mMainFrame.add(mJavaPathLabel);
+        
+        mMainFrame.add(mChooseXmlButton);
+        mMainFrame.add(mXmlPathLabel);
+        
+        mMainFrame.add(mGenerateButton);
+        
+        
+        mJavaFileChooser = new JFileChooser(mOS.getMainDir());
+        mJavaFileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public String getDescription() {
+                return null;
+            }
+            @Override
+            public boolean accept(File f) {
+                if (f.isDirectory())return true;
+                return f.getName().endsWith(".java");
+            }
+        });
+        mXmlFileChooser = new JFileChooser(mOS.getMainDir());
+        mXmlFileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public String getDescription() {
+                return null;
+            }
+            @Override
+            public boolean accept(File f) {
+                if (f.isDirectory())return true;
+                return f.getName().endsWith(".xml");
+            }
+        });
+        
+        mChooseJavaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mJavaFileChooser.showOpenDialog(null);
+                mJavaPathLabel.setText(mJavaFileChooser.getSelectedFile().getAbsolutePath());
+            }
+        });
+        mChooseXmlButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mXmlFileChooser.showOpenDialog(null);
+                mXmlPathLabel.setText(mXmlFileChooser.getSelectedFile().getAbsolutePath());
+            }
+        });
+        mGenerateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (mJavaPathLabel.getText().endsWith(".java") && mXmlPathLabel.getText().endsWith(".xml")) {
+                    mCodeGenerator.generate(mJavaPathLabel.getText(), mXmlPathLabel.getText());
+                }
+            }
+        });
+        
+        mMainFrame.setVisible(true);
+        
+    }
+
+}
