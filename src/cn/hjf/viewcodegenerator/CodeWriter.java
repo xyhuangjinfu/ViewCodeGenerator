493b6ebffdecef45c6f41d4c7b16992af6784b08
huangjinfu <630569027@qq.com>
2015-10-06 15:05:44 +0800

    去除无用的导包

--- src/cn/hjf/viewcodegenerator/CodeWriter.java
+++ src/cn/hjf/viewcodegenerator/CodeWriter.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
 import java.util.List;
 
 import cn.hjf.viewcodegenerator.model.Field;
