628f72257aefddb5d6c179919b43d284f9d3cec4
huangjinfu <xfujohn@xfu.John>
2015-10-08 12:11:00 +0800

    对Adapter生成的代码进行扩充

--- src/cn/hjf/viewcodegenerator/writer/CodeWriter4Adapter.java
+++ src/cn/hjf/viewcodegenerator/writer/CodeWriter4Adapter.java
@@ -22,22 +22,39 @@ public class CodeWriter4Adapter {
         OutputStreamWriter osw = null;
         try {
             osw = new OutputStreamWriter(new FileOutputStream(javaFile, true), "UTF-8");
-            
+
             osw.write("//自动生成代码");
             osw.write("\n");
 
             osw.write("private class ViewHolder {");
             osw.write("\n");
             for (Field field : fields) {
-                osw.write(mStatementCreator.createDeclare(field));
+                osw.write("    " + mStatementCreator.createDeclare(field));
                 osw.write("\n");
             }
             osw.write("}");
             osw.write("\n");
+
+            osw.write("ViewHolder holder;");
+            osw.write("\n");
+            osw.write("if (convertView == null) {");
+            osw.write("\n");
+            osw.write("    holder = new ViewHolder();");
+            osw.write("\n");
+            osw.write("    convertView = LayoutInflater.from(mContext).inflate(R.layout.layout, parent, false);");
+            osw.write("\n");
             for (Field field : fields) {
-                osw.write(mStatementCreator.createFindViewById(field));
+                osw.write("    " + mStatementCreator.createFindViewById(field));
                 osw.write("\n");
             }
+            osw.write("    convertView.setTag(holder);");
+            osw.write("\n");
+            osw.write("} else {");
+            osw.write("\n");
+            osw.write("    holder = (ViewHolder) convertView.getTag();");
+            osw.write("\n");
+            osw.write("}");
+            osw.write("\n");
             osw.flush();
         } catch (Exception e) {
             result = false;
